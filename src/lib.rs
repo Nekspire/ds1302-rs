f473d8f4cf6ea7bbd750a98521907172f448991a
apeng2012 <peng89769975@163.com>
2021-11-24 20:20:53 +0800

    Optimize the representation of Hours

--- src/lib.rs
+++ src/lib.rs
@@ -34,52 +34,39 @@
 //!  # Read time and date
 //! ```
 //! let mut data = String::<U32>::from(" ");
-//! let mut text = " ";
 //!
 //! let cl = ds1302.get_clock_calendar().unwrap();
-//! // Check the current mode. If it is Hour12, check AM/PM value. Please refer to table description below.
-//! match ds1302.mode {
-//!     ds1302_mode::Hour12 => {
-//!         if cl.0.hours.am_pm == 1 {text = " PM"}
-//!         else {text = " AM"}
-//!     }
-//!     ds1302_mode::Hour24 => text = ""
-//! }
+//! let (text, h) = match cl.0.hours {
+//!     Hours::Hour24(h) => ("", h),
+//!     Hours::Hour12am(h) => ("am", h),
+//!     Hours::Hour12pm(h) => ("pm", h),
+//! };
 //! // Glue cl reads in string called "data", and use it later ...
 //! let _=write!(data,"{} {}.{}.{}\n{:02}:{:02}:{:02} {}",
 //!             cl.1.day, cl.1.date, cl.1.month, cl.1.year,
-//!             cl.0.hours.hours, cl.0.minutes, cl.0.seconds, text);
+//!             h, cl.0.minutes, cl.0.seconds, text);
 //! ```
 //!
 //!
 //! From lib.rs:
 //! ```
-//! pub struct Hours {
-//!     pub hours: u8,
-//!     pub am_pm: u8,
+//! pub enum Hours {
+//!     Hour24(u8),
+//!     Hour12am(u8),
+//!     Hour12pm(u8),
 //! }
-//!
 //! pub enum Mode {
 //!     Hour24,
 //!     Hour12,
 //! }
 //! ```
-//! Variants of time format depending on Mode::Hour24, Mode::Hour12 and Hours::am_mp
-//!
-//! Mode | am_pm | time format
-//! --- | --- | ---
-//! Hour12 | 0 | AM
-//! Hour12 | 1 | PM
-//! Hour24 | 0 | -
-//! Hour24 | 1 | -
 //!
 //!
 //! # Set time and date
 //!
 //! ```
-//! let h = Hours {hours: 4, am_pm: 1};
 //! let clk = Clock {
-//!     hours: h,
+//!     hours: Hours::Hour12pm(4),
 //!     minutes: 29,
 //!     seconds: 0
 //! };
@@ -97,6 +84,7 @@
 //!
 #![no_std]
 
+use core::convert::From;
 use embedded_hal as hal;
 use fugit::ExtU32;
 use hal::blocking::spi;
@@ -136,19 +124,66 @@ where
 {
     spi: SPI,
     cs: CS,
-    pub mode: Mode,
     timer: CLK,
 }
 ///Hour format: 12-hour (AM/PM) or 24-hour
+#[derive(PartialEq)]
 pub enum Mode {
     Hour24,
     Hour12,
 }
 ///Hour information: 12-hour (AM/PM) or 24-hour
-pub struct Hours {
-    pub hours: u8,
-    pub am_pm: u8,
+pub enum Hours {
+    Hour24(u8),
+    Hour12am(u8),
+    Hour12pm(u8),
 }
+
+impl Hours {
+    fn convert(&self) -> Self {
+        match *self {
+            Hours::Hour24(h) => {
+                if h >= 12 {
+                    Hours::Hour12pm(h - 12)
+                } else {
+                    Hours::Hour12am(h)
+                }
+            }
+            Hours::Hour12pm(h) => Hours::Hour24(h + 12),
+            Hours::Hour12am(h) => Hours::Hour24(h),
+        }
+    }
+}
+
+impl From<u8> for Hours {
+    fn from(byte: u8) -> Self {
+        if (byte & HOUR_12_BIT) != 0 {
+            //In case 12-hour format
+            let hour = bcd_to_decimal(byte & (!(HOUR_12_BIT | HOUR_PM_BIT)));
+            if (byte & HOUR_PM_BIT) != 0 {
+                // It's PM
+                Hours::Hour12pm(hour)
+            } else {
+                // It's AM
+                Hours::Hour12am(hour)
+            }
+        } else {
+            let hour = bcd_to_decimal(byte);
+            Hours::Hour24(hour)
+        }
+    }
+}
+
+impl From<Hours> for u8 {
+    fn from(h: Hours) -> Self {
+        match h {
+            Hours::Hour24(hour) => decimal_to_bcd(hour),
+            Hours::Hour12am(hour) => decimal_to_bcd(hour) | HOUR_12_BIT,
+            Hours::Hour12pm(hour) => decimal_to_bcd(hour) | HOUR_12_BIT | HOUR_PM_BIT,
+        }
+    }
+}
+
 ///Clock information
 pub struct Clock {
     pub hours: Hours,
@@ -173,12 +208,7 @@ where
 {
     ///Creates new instance DS1302 RTC
     pub fn new(spi: SPI, cs: CS, mode: Mode, timer: CLK) -> Result<Self, E> {
-        let mut ds1302 = DS1302 {
-            spi,
-            cs,
-            mode: Mode::Hour12,
-            timer,
-        };
+        let mut ds1302 = DS1302 { spi, cs, timer };
         // Check CLOCK HALT FLAG bit
         let byte = ds1302.read_reg(Register::SECONDS.addr())?;
         // Reset CLOCK HALT FLAG bit, power on device
@@ -212,7 +242,6 @@ where
     }
 
     fn write_reg(&mut self, reg: u8, byte: u8) -> Result<(), E> {
-        self.bcd_to_decimal(byte)?;
         //Firstly Check WRITE_PROTECT_BIT
         let wp_read = self.read_reg(Register::WP.addr())?;
         if (wp_read & WRITE_PROTECT_BIT) != 0 {
@@ -232,61 +261,40 @@ where
         self.timer.start(4.micros()).ok();
         Ok(())
     }
-    // Swap format from bcd to decmial
-    fn bcd_to_decimal(&mut self, bcd: u8) -> Result<u8, E> {
-        Ok(((bcd & 0xF0) >> 4) * 10 + (bcd & 0x0F))
-    }
-    // Swap format from decimal to bcd
-    fn decimal_to_bcd(&mut self, decimal: u8) -> Result<u8, E> {
-        Ok(((decimal / 10) << 4) + (decimal % 10))
-    }
+
     ///Return current information about seconds
     pub fn get_seconds(&mut self) -> Result<u8, E> {
-        let byte = self.read_reg(Register::SECONDS.addr())?;
-        self.bcd_to_decimal(byte)
+        self.read_reg(Register::SECONDS.addr())
+            .map(|b| bcd_to_decimal(b))
     }
     ///Return current information about minutes
     pub fn get_minutes(&mut self) -> Result<u8, E> {
-        let byte = self.read_reg(Register::MINUTES.addr())?;
-        self.bcd_to_decimal(byte)
+        self.read_reg(Register::MINUTES.addr())
+            .map(|b| bcd_to_decimal(b))
     }
     ///Return current information about hours
     pub fn get_hours(&mut self) -> Result<Hours, E> {
-        let mut hr = Hours { hours: 0, am_pm: 0 };
-        let mut byte = self.read_reg(Register::HOURS.addr())?;
-        if (byte & HOUR_12_BIT) != 0 {
-            //In case 12-hour format
-            if (byte & HOUR_PM_BIT) != 0 {
-                // It's PM
-                byte &= !(HOUR_12_BIT | HOUR_PM_BIT); // Clear 7th and 5th bits to designate hours
-                hr.am_pm = 1;
-            } else {
-                // It's AM
-                byte &= !(HOUR_12_BIT); // Clear 7th bit to designate hours
-            }
-        }
-        hr.hours = self.bcd_to_decimal(byte)?;
-        Ok(hr)
+        self.read_reg(Register::HOURS.addr()).map(|b| b.into())
     }
     ///Return current information about date
     pub fn get_date(&mut self) -> Result<u8, E> {
-        let byte = self.read_reg(Register::DATE.addr())?;
-        self.bcd_to_decimal(byte)
+        self.read_reg(Register::DATE.addr())
+            .map(|b| bcd_to_decimal(b))
     }
     ///Return current information about month
     pub fn get_month(&mut self) -> Result<u8, E> {
-        let byte = self.read_reg(Register::MONTH.addr())?;
-        self.bcd_to_decimal(byte)
+        self.read_reg(Register::MONTH.addr())
+            .map(|b| bcd_to_decimal(b))
     }
     ///Return current information about year
     pub fn get_year(&mut self) -> Result<u16, E> {
-        let byte = self.read_reg(Register::YEAR.addr())?;
-        Ok(2000_u16 + (self.bcd_to_decimal(byte)? as u16))
+        self.read_reg(Register::YEAR.addr())
+            .map(|b| 2000_u16 + (bcd_to_decimal(b) as u16))
     }
     ///Return current information about day of the week
     pub fn get_day(&mut self) -> Result<u8, E> {
-        let byte = self.read_reg(Register::DAY.addr())?;
-        self.bcd_to_decimal(byte)
+        self.read_reg(Register::DAY.addr())
+            .map(|b| bcd_to_decimal(b))
     }
     ///Return current information about hours, minutes and seconds
     pub fn get_clock(&mut self) -> Result<Clock, E> {
@@ -298,23 +306,10 @@ where
         self.cs.set_low().ok();
         self.timer.start(4.micros()).ok();
 
-        let mut hr = Hours { hours: 0, am_pm: 0 };
-        if (bytes[3] & HOUR_12_BIT) != 0 {
-            //In case 12-hour format
-            if (bytes[3] & HOUR_PM_BIT) != 0 {
-                // It's PM
-                bytes[3] &= !(HOUR_12_BIT | HOUR_PM_BIT); // Clear 7th and 5th bits to designate hours
-                hr.am_pm = 1;
-            } else {
-                // It's AM
-                bytes[3] &= !(HOUR_12_BIT); // Clear 7th bit to designate hours
-            }
-        }
-        hr.hours = self.bcd_to_decimal(bytes[3])?;
         let clock = Clock {
-            seconds: self.bcd_to_decimal(bytes[1])?,
-            minutes: self.bcd_to_decimal(bytes[2])?,
-            hours: hr,
+            seconds: bcd_to_decimal(bytes[1]),
+            minutes: bcd_to_decimal(bytes[2]),
+            hours: bytes[3].into(),
         };
 
         Ok(clock)
@@ -330,10 +325,10 @@ where
         self.timer.start(4.micros()).ok();
 
         let calendar = Calendar {
-            date: self.bcd_to_decimal(bytes[4])?,
-            day: self.bcd_to_decimal(bytes[5])?,
-            month: self.bcd_to_decimal(bytes[6])?,
-            year: (2000_u16 + (self.bcd_to_decimal(bytes[7])? as u16)),
+            date: bcd_to_decimal(bytes[4]),
+            day: bcd_to_decimal(bytes[5]),
+            month: bcd_to_decimal(bytes[6]),
+            year: (2000_u16 + (bcd_to_decimal(bytes[7]) as u16)),
         };
 
         Ok(calendar)
@@ -348,97 +343,56 @@ where
         self.cs.set_low().ok();
         self.timer.start(4.micros()).ok();
 
-        let mut hr = Hours { hours: 0, am_pm: 0 };
-        if (bytes[3] & HOUR_12_BIT) != 0 {
-            //In case 12-hour format
-            if (bytes[3] & HOUR_PM_BIT) != 0 {
-                // It's PM
-                bytes[3] &= !(HOUR_12_BIT | HOUR_PM_BIT); // Clear 7th and 5th bits to designate hours
-                hr.am_pm = 1;
-            } else {
-                // It's AM
-                bytes[3] &= !(HOUR_12_BIT); // Clear 7th bit to designate hours
-            }
-        }
-        hr.hours = self.bcd_to_decimal(bytes[3])?;
         let clock = Clock {
-            seconds: self.bcd_to_decimal(bytes[1])?,
-            minutes: self.bcd_to_decimal(bytes[2])?,
-            hours: hr,
+            seconds: bcd_to_decimal(bytes[1]),
+            minutes: bcd_to_decimal(bytes[2]),
+            hours: bytes[3].into(),
         };
 
         let calendar = Calendar {
-            date: self.bcd_to_decimal(bytes[4])?,
-            month: self.bcd_to_decimal(bytes[5])?,
-            day: self.bcd_to_decimal(bytes[6])?,
-            year: (2000_u16 + (self.bcd_to_decimal(bytes[7])? as u16)),
+            date: bcd_to_decimal(bytes[4]),
+            month: bcd_to_decimal(bytes[5]),
+            day: bcd_to_decimal(bytes[6]),
+            year: (2000_u16 + (bcd_to_decimal(bytes[7]) as u16)),
         };
 
         Ok((clock, calendar))
     }
     ///Set seconds to defined value
     pub fn set_seconds(&mut self, seconds: u8) -> Result<(), E> {
-        let byte = self.decimal_to_bcd(seconds)?;
-        self.write_reg(Register::SECONDS.addr(), byte)?;
-        Ok(())
+        self.write_reg(Register::SECONDS.addr(), decimal_to_bcd(seconds))
     }
     ///Set minutes to defined value
     pub fn set_minutes(&mut self, minutes: u8) -> Result<(), E> {
-        let byte = self.decimal_to_bcd(minutes)?;
-        self.write_reg(Register::MINUTES.addr(), byte)?;
-        Ok(())
+        self.write_reg(Register::MINUTES.addr(), decimal_to_bcd(minutes))
     }
     ///Set hours to defined value
     pub fn set_hours(&mut self, hours: Hours) -> Result<(), E> {
-        let mut byte = self.decimal_to_bcd(hours.hours)?;
-        match self.mode {
-            Mode::Hour12 => {
-                if hours.am_pm == 1 {
-                    byte |= HOUR_PM_BIT | HOUR_12_BIT;
-                } else {
-                    byte |= HOUR_12_BIT;
-                }
-            }
-            Mode::Hour24 => {}
-        }
-        self.write_reg(Register::HOURS.addr(), byte)?;
-        Ok(())
+        self.write_reg(Register::HOURS.addr(), hours.into())
     }
     ///Set date to defined value
     pub fn set_date(&mut self, date: u8) -> Result<(), E> {
-        let byte = self.decimal_to_bcd(date)?;
-        self.write_reg(Register::DATE.addr(), byte)?;
-        Ok(())
+        self.write_reg(Register::DATE.addr(), decimal_to_bcd(date))
     }
     ///Set month to defined value
     pub fn set_month(&mut self, month: u8) -> Result<(), E> {
-        let byte = self.decimal_to_bcd(month)?;
-        self.write_reg(Register::MONTH.addr(), byte)?;
-        Ok(())
+        self.write_reg(Register::MONTH.addr(), decimal_to_bcd(month))
     }
     ///Set day of the week to defined value
     pub fn set_day(&mut self, day: u8) -> Result<(), E> {
-        let byte = self.decimal_to_bcd(day)?;
-        self.write_reg(Register::DAY.addr(), byte)?;
-        Ok(())
+        self.write_reg(Register::DAY.addr(), decimal_to_bcd(day))
     }
     ///Set year to defined value
-    pub fn set_year(&mut self, mut year: u16) -> Result<(), E> {
-        if year < 2000 {
-            year = 2000
-        }
-        year -= 2000;
-        let byte = self.decimal_to_bcd(year as u8)?;
-        self.write_reg(Register::YEAR.addr(), byte)?;
-        Ok(())
+    pub fn set_year(&mut self, year: u16) -> Result<(), E> {
+        let y = if year < 2000 { 0 } else { year - 2000 };
+        self.write_reg(Register::YEAR.addr(), decimal_to_bcd(y as u8))
     }
     ///Set clock to defined values
     pub fn set_clock(&mut self, clock: Clock) -> Result<(), E> {
         //Not burst mode, because it changes the calendar registers
         self.set_hours(clock.hours)?;
         self.set_minutes(clock.minutes)?;
-        self.set_seconds(clock.seconds)?;
-        Ok(())
+        self.set_seconds(clock.seconds)
     }
     ///Set calendar to defined values
     pub fn set_calendar(&mut self, calendar: Calendar) -> Result<(), E> {
@@ -446,35 +400,26 @@ where
         self.set_year(calendar.year)?;
         self.set_month(calendar.month)?;
         self.set_date(calendar.date)?;
-        self.set_day(calendar.day)?;
-        Ok(())
+        self.set_day(calendar.day)
     }
     ///Set clock and calendar to defined values
-    pub fn set_clock_calendar(&mut self, clock: Clock, mut calendar: Calendar) -> Result<(), E> {
+    pub fn set_clock_calendar(&mut self, clock: Clock, calendar: Calendar) -> Result<(), E> {
         //Writing in burst mode, it changes all the clock and calendar registers
         let mut bytes = [0_u8; 9];
         bytes[0] = Register::CLKBURS.addr();
-        bytes[1] = self.decimal_to_bcd(clock.seconds)?;
-        bytes[2] = self.decimal_to_bcd(clock.minutes)?;
-        bytes[3] = self.decimal_to_bcd(clock.hours.hours)?;
-        match self.mode {
-            Mode::Hour12 => {
-                if clock.hours.am_pm == 1 {
-                    bytes[3] |= HOUR_PM_BIT | HOUR_12_BIT;
-                } else {
-                    bytes[3] |= HOUR_12_BIT;
-                }
-            }
-            Mode::Hour24 => {}
-        }
-        bytes[4] = self.decimal_to_bcd(calendar.date)?;
-        bytes[5] = self.decimal_to_bcd(calendar.month)?;
-        bytes[6] = self.decimal_to_bcd(calendar.day)?;
-        if calendar.year < 2000 {
-            calendar.year = 2000
-        }
-        calendar.year -= 2000;
-        bytes[7] = self.decimal_to_bcd(calendar.year as u8)?;
+        bytes[1] = decimal_to_bcd(clock.seconds);
+        bytes[2] = decimal_to_bcd(clock.minutes);
+        bytes[3] = clock.hours.into();
+        bytes[4] = decimal_to_bcd(calendar.date);
+        bytes[5] = decimal_to_bcd(calendar.month);
+        bytes[6] = decimal_to_bcd(calendar.day);
+        let y = if calendar.year < 2000 {
+            0
+        } else {
+            calendar.year - 2000
+        };
+        bytes[7] = decimal_to_bcd(y as u8);
+
         nb::block!(self.timer.wait()).ok(); // wait CE inactive time min 4us
         self.cs.set_high().ok();
         self.spi.write(&mut bytes)?;
@@ -485,36 +430,31 @@ where
     ///Switch between 12-hour (AM/PM) and 24-hour mode
     pub fn set_clock_mode(&mut self, mode: Mode) -> Result<(), E> {
         let hr = self.get_hours()?; // save current hours data
-        match mode {
-            Mode::Hour12 => {
-                self.write_reg(Register::HOURS.addr(), HOUR_12_BIT)?;
+        match hr {
+            Hours::Hour24(_h) => {
+                if mode == Mode::Hour12 {
+                    self.set_hours(hr.convert())
+                } else {
+                    Ok(())
+                }
             }
-            Mode::Hour24 => {
-                self.write_reg(Register::HOURS.addr(), 0)?;
+            _ => {
+                if mode == Mode::Hour24 {
+                    self.set_hours(hr.convert())
+                } else {
+                    Ok(())
+                }
             }
-        };
-        let hr_new_mode = change_hour_to(&mode, hr);
-        self.mode = mode;
-        self.set_hours(hr_new_mode)?;
-        Ok(())
+        }
     }
 }
 
-fn change_hour_to(mode: &Mode, mut hour: Hours) -> Hours {
-    match mode {
-        Mode::Hour24 => {
-            if hour.am_pm == 1 {
-                hour.hours += 12;
-                hour.am_pm = 0;
-            }
-            hour
-        }
-        Mode::Hour12 => {
-            if hour.hours > 12 {
-                hour.hours -= 12;
-                hour.am_pm = 1;
-            }
-            hour
-        }
-    }
+// Swap format from bcd to decmial
+fn bcd_to_decimal(bcd: u8) -> u8 {
+    ((bcd & 0xF0) >> 4) * 10 + (bcd & 0x0F)
+}
+
+// Swap format from decimal to bcd
+fn decimal_to_bcd(decimal: u8) -> u8 {
+    ((decimal / 10) << 4) + (decimal % 10)
 }
